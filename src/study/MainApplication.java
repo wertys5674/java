9e58e33e0af9ece6fee84abc45f9444e975cc14a
wertys5674 <wertys5674@naver.com>
2021-03-27 20:47:10 +0900

    feat : print 메시지 테스트

--- src/study/MainApplication.java
+++ src/study/MainApplication.java
@@ -2,6 +2,8 @@ package study;
 
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("Hello");
     }
 }
+
+// commit 여러번 해도됨 -> push
